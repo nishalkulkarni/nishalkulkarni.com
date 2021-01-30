81e7d86f1f52b1cc4d46ef4a58edac4bfb19f723
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-01-30 21:37:47 +0530

    Start here

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,22 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { useSiteMetadata } from "../hooks/useSiteMetadata"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import styled from "styled-components"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const StyledH1 = styled.h1`
+  color: rebeccapurple;
+`
+
+const IndexPage = () => {
+  const { title, description } = useSiteMetadata()
+  return (
+    <Layout>
+      <StyledH1>Hi {title}</StyledH1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <p>{description}</p>
+    </Layout>
+  )
+}
 
 export default IndexPage
