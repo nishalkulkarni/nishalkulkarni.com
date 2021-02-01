0bcc50fca0be92f7249e326fcd0a48101ac334f5
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-02-01 22:01:45 +0530

    Added a11y skip to main link

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,26 @@ import GlobalStyles from "../styles/global"
 import Header from "./header"
 import Footer from "./footer"
 
+const A11ySkipLink = styled.a`
+  position: absolute;
+  top: -6rem;
+  left: 0;
+  z-index: 100;
+  padding: 0.5rem;
+  text-align: center;
+  background: ${props => props.theme.primaryBgColor};
+  color: ${props => props.theme.textWeight1};
+
+  :focus {
+    top: 0.75rem;
+  left: 0.75rem;
+    transition: 0.4s top;
+    border: 0.125rem solid ${props => props.theme.textWeight2};
+    text-decoration: underline;
+    color: ${props => props.theme.textWeight1};
+  }
+`
+
 const AppContainer = styled.div`
   background: ${props => props.theme.primaryBgColor};
   border-left: 0.125rem solid ${props => props.theme.pageBorderColor};
@@ -25,11 +45,12 @@ const Main = styled.main`
 const Layout = ({ children }) => {
   const { title, authorGithub } = useSiteMetadata()
   const [theme, toggleTheme] = useDarkMode()
-  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+  const themeMode = theme === "light" ? lightTheme : darkTheme
 
   return (
     <ThemeProvider theme={themeMode}>
       <GlobalStyles />
+      <A11ySkipLink href="#maincontent">Skip to main content</A11ySkipLink>
       <AppContainer>
         <Header
           siteTitle={title}
@@ -37,7 +58,7 @@ const Layout = ({ children }) => {
           theme={theme}
           toggleTheme={toggleTheme}
         />
-        <Main>{children}</Main>
+        <Main id="maincontent">{children}</Main>
         <Footer siteTitle={title} />
       </AppContainer>
     </ThemeProvider>
