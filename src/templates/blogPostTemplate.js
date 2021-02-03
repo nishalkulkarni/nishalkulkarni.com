e035a2de752fd2006ca046857f98a771a7ace1df
Nishal Kulkarni <nishalkulkarni@gmail.com>
2021-02-03 18:50:09 +0530

    Fixed missing branch on github link

--- src/templates/blogPostTemplate.js
+++ src/templates/blogPostTemplate.js
@@ -83,7 +83,7 @@ export default ({ data }) => {
             Found something wrong or missing?{" "}
             <a
               href={
-                "https://github.com/nishalkulkarni/nishalkulkarni.com/blob/master/posts/" +
+                "https://github.com/nishalkulkarni/nishalkulkarni.com/blob/main/posts/" +
                 slug +
                 "index.mdx"
               }
